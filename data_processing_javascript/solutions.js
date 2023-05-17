c319c6ce8a7f875807f6ba0f47dc00a81cd203f8
K. Brady Christianson <a02347230@usu.edu>
2023-05-17 16:22:33 -0600

    first draft of filter is complete with integrated tests.

--- data_processing_javascript/solutions.js
+++ data_processing_javascript/solutions.js
@@ -1,2 +1,51 @@
+// make sure the data is read
 console.log(transactions);
 console.log(customers);
+
+// Modular functions
+
+// filter function
+function filter(data, predicate) {
+    const filteredArray = [];
+      for(const i of data) {
+        if(predicate) filteredArray.push(data[i]);
+      }
+    return filteredArray;
+}
+
+// Filter tests
+const data = [1,2,3,4,undefined,5,6,7,0,0,"Hey bud!", null, `You are uncooth`, 'Bee yourself', "",``,'',true,false, x => x === "this is a test"];
+const modResult = filter(data, i => i % 2 === 0);
+const stringResult = filter(data, i => typeof i === 'string');
+const undefinedResult = filter(data, i => typeof i === 'undefined');
+const nullResult = filter(data, i => i === null);
+const boolResult = filter(data, i => typeof i === 'boolean');
+const funcResult = filter(data, i => typeof i === 'function')
+console.log(result); // [2,4,6];
+console.log(stringResult); // ["Hey bud!",`You are uncooth`,'Bee yourself',"",``,''];
+console.log(undefinedResult); // [undefined];
+console.log(nullResult); // [null];
+console.log(boolResult); // [true, false];
+console.log(funcResult); // [x === "this is a test"];
+
+
+
+// // findLast function
+// function findLast(data, predicate) {
+    
+// }
+
+// // map function
+// function map(data, callback) {
+
+// }
+
+// // pairIf function
+// function pairIf(data1, data2, callback) {
+
+// }
+
+// // reduce function
+// function reduce(data1, reducer, initialValue) {
+
+// }
