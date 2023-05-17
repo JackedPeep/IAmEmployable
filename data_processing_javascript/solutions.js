3cfccf888cdd2c52708f509139da99ef7f206dc0
K. Brady Christianson <a02347230@usu.edu>
2023-05-17 16:40:48 -0600

    first draft of filter is complete with integrated tests.

--- data_processing_javascript/solutions.js
+++ data_processing_javascript/solutions.js
@@ -1,11 +1,11 @@
 // make sure the data is read
-console.log(transactions);
-console.log(customers);
-
+// console.log(transactions);
+// console.log(customers);
 // Modular functions
 
 // filter function
 function filter(data, predicate) {
+    debugger;
     const filteredArray = [];
       for(const i of data) {
         if(predicate) filteredArray.push(data[i]);
@@ -20,7 +20,7 @@ const stringResult = filter(data, i => typeof i === 'string');
 const undefinedResult = filter(data, i => typeof i === 'undefined');
 const nullResult = filter(data, i => i === null);
 const boolResult = filter(data, i => typeof i === 'boolean');
-const funcResult = filter(data, i => typeof i === 'function')
+const funcResult = filter(data, i => typeof i === 'function');
 console.log(result); // [2,4,6];
 console.log(stringResult); // ["Hey bud!",`You are uncooth`,'Bee yourself',"",``,''];
 console.log(undefinedResult); // [undefined];
