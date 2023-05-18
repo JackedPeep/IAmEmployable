0dbc478d3b1f56065eda0f22675c409f15314ec9
K. Brady Christianson <a02347230@usu.edu>
2023-05-17 19:44:33 -0600

    Finished the filter function in solutions.js and test modules work perfectly. The company just needs to develope good predicate functions as modular components to filter the data as needed.

--- data_processing_javascript/solutions.js
+++ data_processing_javascript/solutions.js
@@ -4,24 +4,30 @@ console.log(customers);
 // Modular functions
 
 // filter function
-function filter(data, predicate) {
-    debugger;
+function myFilter(data, predicate) {
     const filteredArray = [];
-      for(const i of data) {
-        if(predicate) filteredArray.push(data[i]);
+      for(const value of data) {
+        if(predicate(value)) {
+          filteredArray.push(value);
+        } else {
+          console.log("This is nor the data we are looking for...");
+        }
       }
     return filteredArray;
 }
 
 // Filter tests
 const data = [1,2,3,4,undefined,5,6,7,0,0,"Hey bud!", null, `You are uncooth`, 'Bee yourself', "",``,'',true,false, x => x === "this is a test"];
-const modResult = filter(data, i => i % 2 === 0);
-const stringResult = filter(data, i => typeof i === 'string');
-const undefinedResult = filter(data, i => typeof i === 'undefined');
-const nullResult = filter(data, i => i === null);
-const boolResult = filter(data, i => typeof i === 'boolean');
-const funcResult = filter(data, i => typeof i === 'function');
-console.log(result); // [2,4,6];
+const modResult = myFilter(data, i => i % 2 === 0);
+const stringResult = myFilter(data, i => typeof i === 'string');
+const undefinedResult = myFilter(data, i => typeof i === 'undefined');
+const nullResult = myFilter(data, i => i === null);
+const boolResult = myFilter(data, i => typeof i === 'boolean');
+const funcResult = myFilter(data, i => typeof i === 'function');
+
+console.log(`My test array: ${data}`);
+console.log(`My test array length: ${data.length}`)
+console.log(modResult); // [2,4,6,0,0];
 console.log(stringResult); // ["Hey bud!",`You are uncooth`,'Bee yourself',"",``,''];
 console.log(undefinedResult); // [undefined];
 console.log(nullResult); // [null];
