c49033473c2ae9fdc4139ea7dc86321353ed99de
K. Brady Christianson <a02347230@usu.edu>
2023-11-20 22:33:20 -0700

    I added audio Qs to the animal buttons as well as scraped the old site stuff. The fractal generator is busted again, and I will have to debug.

--- fractal_factory_0.0.1/factoryInterface.js
+++ fractal_factory_0.0.1/factoryInterface.js
@@ -1,85 +1,73 @@
-// Get inputs
-let colorInputs = document.querySelectorAll('.color');
-let originSelector = document.getElementById('origin-selector');
-let selectedorigin = originSelector.value;
-let shapeSelector = document.getElementById('shape-selector');
-let shape = shapeSelector.value;
+// define colors
+const colors = {
+  'red': [255, 0, 0],
+  'orange': [255, 127, 0],
+  'yellow': [255, 255, 0],
+  'green': [0, 255, 0],
+  'blue': [0, 0, 255],
+  'violet': [238, 130, 238],
+  'UV': [255, 255, 255]
+};
 
-// Get 'Create' button
-let createButton = document.querySelector('#create-button');
+// Define your color gradients
+const colorGradients = {
+  'human-button': [colors['red'], colors['orange'], colors['yellow'], colors['green'], colors['blue'], colors['violet']], // Human R,G,B
+  'cat-button': [colors['yellow'], colors['blue']], // Cat B,Y
+  'humming-button': [colors['red'], colors['orange'], colors['yellow'], colors['green'], colors['blue'], colors['violet'], colors['UV']], // Humming Bird R,G,B,UV
+  'golden-button': [[0, 0, 0]], // Golden Mole ...
+  'starfish-button': [[255, 255, 255]], // Starfish SL
+  'bee-button': [colors['yellow'], colors['blue'], colors['UV']], // Bee B,Y,UV
+  'rabbit-button': [colors['green'], colors['blue']], // Rabbit B,G
+  'rat-button': [colors['green'], colors['blue'], colors['UV']] // Rat B,G,UV
+};
 
-// Add event listener to 'Create' button
-createButton.addEventListener('click', function() {
-  //clear canvas
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  // Get selected origin and colors when the button is clicked
-  let selectedOrigin = originSelector.value;
-  let colors = Array.from(colorInputs).map(input => hexToRgb(input.value));
-  let shape = shapeSelector.value;
-// Set the initial origin based on the selected origin
-  let x, y;
-  switch(selectedOrigin) {
-    case 'top-left':
-      x = y = len * .025; //len*.025 gives us a margin of 2.5% the canvas size
-      break;
-    case 'top-center':
-      x = canvas.width / 2;
-      y = len * .025; 
-      break;
-    case 'top-right':
-      x = canvas.width - len * .025; 
-      y = len * .025; 
-      break;
-    case 'center-left':
-      x = len * .025; 
-      y = canvas.height / 2;
-      break;
-    case 'center':
-      x = canvas.width / 2;
-      y = canvas.height / 2;
-      break;
-    case 'center-right':
-      x = canvas.width - len * .025; 
-      y = canvas.height / 2;
-      break;
-    case 'bottom-left':
-      x = len * .025; 
-      y = canvas.height - len * .025; 
-      break;
-    case 'bottom-center':
-      x = canvas.width / 2;
-      y = canvas.height - len * .025; 
-      break;
-    case 'bottom-right':
-      x = canvas.width - len * .025; 
-      y = canvas.height - len * .025; 
-      break;
-  }
-  // Call your function to draw the fractal
-  drawFractal(x, y, len, angle, colors, shape, 0);
-});
+// Define your audio files
+const audioFiles = {
+  'human-button': new Audio('audio/scribble.mp3'),
+  'humming-button': new Audio('audio/br_hummers1.mp3'),
+  'cat-button': new Audio('audio/cat-meow.mp3'),
+  'golden-button': new Audio('audio/sandfall.mp3'),
+  'starfish-button': new Audio('audio/underwater-waves.mp3'),
+  'bee-button': new Audio('audio/bumblebee.mp3'),
+  'rabbit-button': new Audio('audio/boing-spring-mouth-harp.mp3'),
+  'rat-button': new Audio('audio/double-squeak.mp3')
+};
 
-// Get the canvas and context
+
+
+// Get the element where you want to display the color gradient
+let gradientElement = document.getElementById('gradient');
 let canvas = document.getElementById('fractal-window');
 let ctx = canvas.getContext('2d');
+let len = 200
+let shape = 'shape-line';
+let angle = Math.PI/2
+const x = canvas.width / 2;
+const y = canvas.height - len * .025; 
 
-// Set the initial position, length and angle
-let x = canvas.width / 2;
-let y = canvas.height;
-let len = 100;
-let angle = -Math.PI / 2;
+// Add event listeners to the buttons
+Object.keys(audioFiles).forEach(function(buttonId) {
+    document.getElementById(buttonId).addEventListener('click', function() {
+        // Stop all audio
+        Object.values(audioFiles).forEach(function(audio) {
+            audio.pause();
+            audio.currentTime = 0;
+        });
 
-// Clear the canvas and set the stroke color
-ctx.clearRect(0, 0, canvas.width, canvas.height);
-ctx.strokeStyle = '#ffffff';
+        // Play the audio for this button
+        audioFiles[buttonId].play();
+
+        // Display the color gradient for this button
+        gradientElement.style.background = colorGradients[buttonId];
+
+        // Call your function to draw the fractal with the new gradient
+        drawFractal(x, y, len, angle, colorGradients[buttonId], shape, 0);
+    });
+});
 
 window.onload = function() {
   // Get selected position and colors
-  let selectedOrigin = originSelector.value;
-  let colors = Array.from(colorInputs).map(input => hexToRgb(input.value));
-  let shape = shapeSelector.value;
-  // Set the initial position based on the selected position
-  // Your existing code...
+  let colors = Array.from(colorGradients["human-button"]);
 
   // Call your function to draw the fractal
   drawFractal(x, y, len, angle, colors, shape, 0);
--- fractal_factory_0.0.1/fractalFactory.js
+++ fractal_factory_0.0.1/fractalFactory.js
@@ -10,33 +10,23 @@ function drawFractal(x, y, len, angle, colors, shape, depth = 0) {
   let x1 = x + len * Math.cos(angle);
   let y1 = y + len * Math.sin(angle);
 
-  // Calculate the color index and interpolation factor
-  let index = Math.floor(depth / len);
-  let factor = (depth % len) / len;
-
-  // Interpolate between the current color and the next color
-  let color1 = colors[index % colors.length];
-  let color2 = colors[(index + 1) % colors.length];
-  let color = interpolateColor(color1, color2, factor);
-
-  // Set the stroke color
-  ctx.strokeStyle = 'rgb(' + color.join(',') + ')';
-
   // Draw the shape
   switch(shape) {
-
     case 'shape-line':
       // Draw each pixel along the line
-      if (depth > 0){
       for(let i = 0; i < len; i++) {
         // Calculate the position of the pixel
         let px = x + i * Math.cos(angle);
         let py = y + i * Math.sin(angle);
 
+        // Calculate the color index and interpolation factor
+        let index = Math.floor((depth + i) / len);
+        let factor = ((depth + i) % len) / len;
+
         // Interpolate between the current color and the next color
         let color1 = colors[index % colors.length];
         let color2 = colors[(index + 1) % colors.length];
-        let color = interpolateColor(color1, color2, i / len);
+        let color = interpolateColor(color1, color2, factor);
 
         // Set the fill color
         ctx.fillStyle = 'rgb(' + color.join(',') + ')';
@@ -44,81 +34,13 @@ function drawFractal(x, y, len, angle, colors, shape, depth = 0) {
         // Draw the pixel
         ctx.fillRect(px, py, 1, 1);
       }
-       // Recursive calls for the two new branches
-    drawFractal(x1, y1, len * 0.75, angle + Math.PI / 6, colors, shape, depth + 1);
-    drawFractal(x1, y1, len * 0.75, angle - Math.PI / 6, colors, shape, depth + 1);
-      };
-      break;
-    case 'shape-triangle':
 
-      if(depth > 0) {
-        // Draw smaller triangles at each corner
-        drawFractal(x, y, len / 2, angle, colors, shape, depth - 1);
-        drawFractal(x + len / 2 * Math.cos(angle), y + len / 2 * Math.sin(angle), len / 2, angle, colors, shape, depth - 1);
-        drawFractal(x + len / 2 * Math.cos(angle - Math.PI / 3), y + len / 2 * Math.sin(angle - Math.PI / 3), len / 2, angle, colors, shape, depth - 1);
-      } else {
-        // Base case: draw a single triangle
-        ctx.beginPath();
-        ctx.moveTo(x, y);
-        ctx.lineTo(x + len * Math.cos(angle), y + len * Math.sin(angle));
-        ctx.lineTo(x + len * Math.cos(angle - Math.PI / 3), y + len * Math.sin(angle - Math.PI / 3));
-        ctx.closePath();
-        ctx.stroke();
-      }
-      break;
-    case 'shape-square':
-
-      if(depth > 0) {
-        // Draw smaller squares at each corner
-        drawFractal(x, y, len / 2, angle, colors, shape, depth - 1);
-        drawFractal(x + len / 2 * Math.cos(angle), y + len / 2 * Math.sin(angle), len / 2, angle, colors, shape, depth - 1);
-        drawFractal(x + len / 2 * Math.cos(angle + Math.PI / 2), y + len / 2 * Math.sin(angle + Math.PI / 2), len / 2, angle, colors, shape, depth - 1);
-        drawFractal(x + len / 2 * Math.cos(angle - Math.PI / 2), y + len / 2 * Math.sin(angle - Math.PI / 2), len / 2, angle, colors, shape, depth - 1);
-      } else {
-        // Base case: draw a single square pixel by pixel
-        for(let i = 0; i < len; i++) {
-          for(let j = 0; j < len; j++) {
-            // Interpolate between the current color and the next color
-            let color1 = colors[index % colors.length];
-            let color2 = colors[(index + 1) % colors.length];
-            let color = interpolateColor(color1, color2, (i * len + j) / (len * len));
-    
-            // Set the fill color
-            ctx.fillStyle = 'rgb(' + color.join(',') + ')';
-    
-            // Draw the pixel
-            ctx.fillRect(x + i, y + j, 1, 1);
-          }
-        }
-      }
-      break;
-    case 'shape-circle':
-
-      if(depth >= 0) {
-        // Draw smaller circles around the current circle
-        drawFractal(x + len / 2 * Math.cos(angle), y + len / 2 * Math.sin(angle), len / 2, angle, colors, shape, depth - 1);
-        drawFractal(x + len / 2 * Math.cos(angle + Math.PI / 2), y + len / 2 * Math.sin(angle + Math.PI / 2), len / 2, angle, colors, shape, depth - 1);
-        drawFractal(x + len / 2 * Math.cos(angle + Math.PI), y + len / 2 * Math.sin(angle + Math.PI), len / 2, angle, colors, shape, depth - 1);
-        drawFractal(x + len / 2 * Math.cos(angle - Math.PI / 2), y + len / 2 * Math.sin(angle - Math.PI / 2), len / 2, angle, colors, shape, depth - 1);
-      } else {
-        // Base case: draw a single circle
-        ctx.beginPath();
-        ctx.arc(x, y, len / 2, 0, 2 * Math.PI);
-        ctx.stroke();
-      }
+      // Recursive calls for the two new branches
+      drawFractal(x1, y1, len * 0.75, angle + Math.PI / 6, colors, shape, depth + len);
+      drawFractal(x1, y1, len * 0.75, angle - Math.PI / 6, colors, shape, depth + len);
       break;
   }
-  };
-
-// Changes a hex color to rgb
-function hexToRgb(hex) {
-  let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-  return result ? [
-    parseInt(result[1], 16),
-    parseInt(result[2], 16),
-    parseInt(result[3], 16)
-  ] : null;
-}
+};
 
 //interpolates from color1 to color2 
 
@@ -129,3 +51,13 @@ function interpolateColor(color1, color2, factor) {
   }
   return result;
 }
+
+// // Changes a hex color to rgb
+// function hexToRgb(hex) {
+//   let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+//   return result ? [
+//     parseInt(result[1], 16),
+//     parseInt(result[2], 16),
+//     parseInt(result[3], 16)
+//   ] : null;
+// }
